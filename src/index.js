bca108540695c4718831b63c0ac9bb2f4f66666a
Ankur Jarial <ankujarialthakur@gmail.com>
2020-09-10 01:29:12 +0530

    Access-Control added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const Taskrouter=require("./routes/tasks");
 const app=express();
 const port=process.env.PORT;
 app.use(express.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  app.use(cors())
 app.use(Taskrouter);
+
 
 
 app.listen(port,()=>{
