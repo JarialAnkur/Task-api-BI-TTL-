129a0b1103c23fe2e133f5e3c6240720cf34a756
Ankur Jarial <ankujarialthakur@gmail.com>
2020-09-10 01:32:55 +0530

    Access-Control added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 require('./db/mongoose');
 const Taskrouter=require("./routes/tasks");
+const cors=require("cors");
 
 const app=express();
 const port=process.env.PORT;
